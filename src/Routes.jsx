722d0c22ecf10250d3435a715d4a320a81d8f9b9
Antonio Goeminne <antonionicolas3003@gmail.com>
2021-08-08 12:00:26 -0300

    trying deploy the app

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import { Home } from "./pages/home/Home"
 import { Projects } from "./pages/projects/Projects"
+import { Logo } from "./components/logo/Logo"
 
-export const Routes = () => (
+export const Routes = () => {
+    return(
     <Router>
         <Switch>
             <Route exact path='/'>
+                <Logo />
                 <Home />
             </Route>
             <Route exact path='/projects'>
@@ -13,4 +16,5 @@ export const Routes = () => (
             </Route>
         </Switch>
     </Router>
-)
+    )
+}
