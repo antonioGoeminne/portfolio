b7d180c55af0a9fd14bcea8a9cc18e7b67b88230
Antonio Goeminne <antonionicolas3003@gmail.com>
2021-12-26 20:59:23 -0300

    adding dyanamai project

--- src/components/ProjectsContainer/ProjectsContainer.jsx
+++ src/components/ProjectsContainer/ProjectsContainer.jsx
@@ -20,8 +20,16 @@ export const ProjectsContainer = () => {
       description:'Practising React using Firebase and Context Api to the Cart and orders from users.',
       technologies:'React, Css, Firebase',
       link:'https://antoniogoeminne.github.io/TiendReact'
-    }
-    ])
+    },
+    {
+    id:2,
+    picture:Dynamai,
+    title:'Web Dynamai',
+    description:'Trabajo colaborativo con diseñadores y programadores para crear la web de una Ong sobre la primera infancia.',
+    technologies:'React, Sass, Firebase, TailWind',
+    link:'https://dynamaiapp.org/'
+  },
+         ])
     return(
       <ProjectList projects={projectInfo}></ProjectList>
     )
